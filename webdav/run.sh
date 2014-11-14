f6b7271e8845a5c454a38b839397595b6a28b3f9
Morris Jobke <hey@morrisjobke.de>
2014-11-14 09:36:15 +0100

    first docker files for webdav

---
+++ webdav/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+htpasswd -cb /etc/apache2/webdav.password webdav webdav
+chown root:www-data /etc/apache2/webdav.password
+chmod 640 /etc/apache2/webdav.password
+
+apache2 -D FOREGROUND
